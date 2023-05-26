58fc91a8434d83019b25577a6a7894f88099d0bb
Juan14042001 <91479241+Juan14042001@users.noreply.github.com>
2023-05-25 23:23:02 -0500

    Update Constantes.js
    Agregando Constante de Credenciales

--- constantes/Constantes.js
+++ constantes/Constantes.js
@@ -7,6 +7,14 @@ class Constantes{
     static get USERS_ADMINS(){
         return ['admin']
     }
+        /**
+     * USERS_ADMINS
+     * @description constante para obtener credenciales
+     * @returns {Array} retorna credenciales
+     */
+    static get CREDENCIUALES(){
+        return ['1']
+    }
 }
 
 module.exports = Constantes
